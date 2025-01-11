d13d9a0fe0d12658e5d5a7375d6d6be7dcd99f90
E. Borodov <mgopjik@gmail.com>
2025-01-11 15:34:06 +0300

    Refactor: merge CustomerEntity and EmployeeEntity into UserEntity

--- Evcomp.Data/Entities/UserEntity.cs
+++ Evcomp.Data/Entities/UserEntity.cs
@@ -2,13 +2,25 @@
 
 namespace Evcomp.API.Models
 {
-    public class CustomerEntity
+    public class UserEntity
     {
         [Required]
         public int Id { get; set; }
+
+        [Required]
         public string FirstName { get; set; } = string.Empty;
+
+        [Required]
         public string LastName { get; set; } = string.Empty;
+
+        [Required]
+        public string PasswordHash { get; set; } = string.Empty;
+
         public string Contacts { get; set; } = string.Empty;
+
+        [Required]
+        public string Role { get; set; } = "Customer";
+
         public ICollection<OrderEntity> Orders { get; set; } = [];
     }
 }
